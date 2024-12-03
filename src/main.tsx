8cfc8068adab87752d89de5aad870511f4cd58a7
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-12-03 22:48:31 +0500

    Adapt triangolo frontend to latest version of hey api client generator

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import ReactDOM from "react-dom/client"
+import { routeTree } from "./routeTree.gen"
+
+import { StrictMode } from "react"
+import { client } from "./client"
+import theme from "./theme"
+
+client.setConfig({
+  // set default base url for requests
+  baseURL: import.meta.env.VITE_API_URL,
+  // set default headers for requests
+  headers: {
+    Authorization: "bearer",
+  },
+});
+
+const queryClient = new QueryClient()
+
+const router = createRouter({ routeTree })
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </StrictMode>,
+)
